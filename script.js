df193334e4ef9d03d03f694487e865b4daf733bf
Dani1324 <97694556+Dani1324@users.noreply.github.com>
2023-09-23 17:35:09 +0200

    1.10

---
+++ script.js
@@ -0,0 +1,63 @@
+// Smooth scrolling to all .smooth links
+document.querySelectorAll("a.smooth").forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        document.documentElement.scrollTo({
+          top: document.querySelector(hash).offsetTop,
+          behavior: 'smooth'
+        });
+        window.location.hash = hash;
+      }
+    });
+  });
+
+  // Swiper settings
+  const width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  if (width > 1025) {
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      keyboard: true,
+      simulateTouch: false,
+      allowTouchMove: false,
+    });
+  }
+  else {
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      keyboard: false,
+      simulateTouch: false,
+      allowTouchMove: false,
+    });
+  }
